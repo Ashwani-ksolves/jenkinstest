a8add1b137735cd30929e12bde8f6d8b11fe0f3e
Ashwani <ashwani.kumar@ksolves.com>
2023-11-07 22:43:54 +0530

    changes

--- jenkinstest/src/main/java/jenkinstest/Jentest.java
+++ jenkinstest/src/main/java/jenkinstest/Jentest.java
@@ -1,16 +1,19 @@
 package jenkinstest;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class Jentest {
 	//WebDriver driver;
-	@BeforeTest
+	@Test
 	public void tt()
 	{
 //	
 //		driver.get("http://lokbest.kappso.com/customer/account/login/");
 //		driver.close();
 		System.out.println("csdds");
+		Assert.assertEquals("sxs","scc" );
 	}
 
 }
