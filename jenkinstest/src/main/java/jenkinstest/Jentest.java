9618204b5b110a7d9feb70dff874547d2f608207
Ashwani <ashwani.kumar@ksolves.com>
2023-11-07 22:18:24 +0530

    Test jenkins

---
+++ jenkinstest/src/main/java/jenkinstest/Jentest.java
@@ -0,0 +1,16 @@
+package jenkinstest;
+
+import org.testng.annotations.BeforeTest;
+
+public class Jentest {
+	//WebDriver driver;
+	@BeforeTest
+	public void tt()
+	{
+//	
+//		driver.get("http://lokbest.kappso.com/customer/account/login/");
+//		driver.close();
+		System.out.println("csdds");
+	}
+
+}
